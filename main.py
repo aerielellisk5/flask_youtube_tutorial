c6b20ca0b68d48e6fcc680fa156c94918ecbb546
Aeriel <aerielellisk5@gmail.com>
2023-05-04 21:03:06 -0700

    created a container with the app using docker

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True, port=3000)
     # this line will rerun the webserver any time we update the code
     # you would turn this off or debug=false in production
